54060b6eb773bc88f34f2a7fbcb8ef3ecaf12d7d
Natália Jobim <natjborges@yahoo.com.br>
2022-11-16 20:39:31 -0300

    Initial commit

---
+++ src/Strategy/Robo.java
@@ -0,0 +1,17 @@
+package Strategy;
+
+public class Robo {
+
+    private Comportamento comportamento;
+
+    public void setStrategy(Comportamento comportamento) {
+        this.comportamento = comportamento;
+    }
+
+    public void mover(){
+        comportamento.mover();
+    }
+
+    public void setComportamento(Comportamento normal) {
+    }
+}
